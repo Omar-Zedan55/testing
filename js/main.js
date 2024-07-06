18fe85fd71baf0a9349e527c1570f06b0e6fa65d
omar-zedan <omar2025zedan@gmail.com>
2024-07-06 04:20:49 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,327 @@
+function alll(){
+    document.getElementById("mygalary").innerHTML=`
+    <div id="mygalary" class=" container-fluid  gallaryyy">
+                <div class="row gal">
+                    <div class="col-sm-12 col-lg-4 col-md-6">
+                        <div class="innerrr-imgg position-relative w-100">
+                            <img src="./images/portfolio/1.jpg" class="w-100" alt="" srcset="">
+                            <div class="innerrr-imgg-layer text-center  ">
+                                <h3 class="wd">WEB DESIGN</h3>
+                                <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon justify-content-center align-items-center">
+                                            <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                                    </a>
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                            <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-md-6">
+                        <div class="innerrr-imgg position-relative w-100">
+                            <img src="./images/portfolio/2.jpg" class="w-100" alt="" srcset="">
+                            <div class="innerrr-imgg-layer text-center  ">
+                                <h3 class="wd">WEB DESIGN</h3>
+                                <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon justify-content-center align-items-center">
+                                            <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                                    </a>
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                            <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-md-6">
+                        <div class="innerrr-imgg position-relative w-100">
+                            <img src="./images/portfolio/3.jpg" class="w-100" alt="" srcset="">
+                            <div class="innerrr-imgg-layer text-center  ">
+                                <h3 class="wd">WEB DESIGN</h3>
+                                <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon justify-content-center align-items-center">
+                                            <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                                    </a>
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                            <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-md-6">
+                        <div class="innerrr-imgg position-relative w-100">
+                            <img src="./images/portfolio/4.jpg" class="w-100" alt="" srcset="">
+                            <div class="innerrr-imgg-layer text-center  ">
+                                <h3 class="wd">WEB DESIGN</h3>
+                                <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon justify-content-center align-items-center">
+                                            <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                                    </a>
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                            <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-md-6">
+                        <div class="innerrr-imgg position-relative w-100">
+                            <img src="./images/portfolio/5.jpg" class="w-100" alt="" srcset="">
+                            <div class="innerrr-imgg-layer text-center  ">
+                                <h3 class="wd">WEB DESIGN</h3>
+                                <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon justify-content-center align-items-center">
+                                            <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                                    </a>
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                            <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-md-6">
+                        <div class="innerrr-imgg position-relative w-100">
+                            <img src="./images/portfolio/6.jpg" class="w-100" alt="" srcset="">
+                            <div class="innerrr-imgg-layer text-center  ">
+                                <h3 class="wd">WEB DESIGN</h3>
+                                <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon justify-content-center align-items-center">
+                                            <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                                    </a>
+                                    <a href="">
+                                        <div
+                                            class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                            <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>`
+}
+
+function brand(){
+    document.getElementById("mygalary").innerHTML=`<div id="mygalary" class=" container-fluid  gallaryyy">
+    <div class="row gal">
+       
+        <div class="col-sm-12 col-lg-4 col-md-6">
+            <div class="innerrr-imgg position-relative w-100">
+                <img src="./images/portfolio/4.jpg" class="w-100" alt="" srcset="">
+                <div class="innerrr-imgg-layer text-center  ">
+                    <h3 class="wd">WEB DESIGN</h3>
+                    <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon justify-content-center align-items-center">
+                                <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                        </a>
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+       
+
+
+        <div class="col-sm-12 col-lg-4 col-md-6">
+            <div class="innerrr-imgg position-relative w-100">
+                <img src="./images/portfolio/6.jpg" class="w-100" alt="" srcset="">
+                <div class="innerrr-imgg-layer text-center  ">
+                    <h3 class="wd">WEB DESIGN</h3>
+                    <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon justify-content-center align-items-center">
+                                <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                        </a>
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>`
+}
+function design(){
+    document.getElementById("mygalary").innerHTML=`<div id="mygalary" class=" container-fluid  gallaryyy">
+    <div class="row gal">
+        
+        <div class="col-sm-12 col-lg-4 col-md-6">
+            <div class="innerrr-imgg position-relative w-100">
+                <img src="./images/portfolio/2.jpg" class="w-100" alt="" srcset="">
+                <div class="innerrr-imgg-layer text-center  ">
+                    <h3 class="wd">WEB DESIGN</h3>
+                    <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon justify-content-center align-items-center">
+                                <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                        </a>
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="col-sm-12 col-lg-4 col-md-6">
+            <div class="innerrr-imgg position-relative w-100">
+                <img src="./images/portfolio/5.jpg" class="w-100" alt="" srcset="">
+                <div class="innerrr-imgg-layer text-center  ">
+                    <h3 class="wd">WEB DESIGN</h3>
+                    <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon justify-content-center align-items-center">
+                                <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                        </a>
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+       
+
+    </div>
+</div>`
+}
+function graphic(){
+    document.getElementById("mygalary").innerHTML=`<div id="mygalary" class=" container-fluid  gallaryyy">
+    <div class="row gal">
+        <div class="col-sm-12 col-lg-4 col-md-6">
+            <div class="innerrr-imgg position-relative w-100">
+                <img src="./images/portfolio/1.jpg" class="w-100" alt="" srcset="">
+                <div class="innerrr-imgg-layer text-center  ">
+                    <h3 class="wd">WEB DESIGN</h3>
+                    <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon justify-content-center align-items-center">
+                                <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                        </a>
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+       
+        <div class="col-sm-12 col-lg-4 col-md-6">
+            <div class="innerrr-imgg position-relative w-100">
+                <img src="./images/portfolio/3.jpg" class="w-100" alt="" srcset="">
+                <div class="innerrr-imgg-layer text-center  ">
+                    <h3 class="wd">WEB DESIGN</h3>
+                    <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon justify-content-center align-items-center">
+                                <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                        </a>
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="col-sm-12 col-lg-4 col-md-6">
+            <div class="innerrr-imgg position-relative w-100">
+                <img src="./images/portfolio/5.jpg" class="w-100" alt="" srcset="">
+                <div class="innerrr-imgg-layer text-center  ">
+                    <h3 class="wd">WEB DESIGN</h3>
+                    <div class="innerrr-imgg-layer-icons d-flex justify-content-center">
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon justify-content-center align-items-center">
+                                <i class="fa fa-chain-broken" aria-hidden="true"></i></div>
+                        </a>
+                        <a href="">
+                            <div
+                                class="rounded-circle layer-icon d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+
+    </div>
+</div>`
+}
